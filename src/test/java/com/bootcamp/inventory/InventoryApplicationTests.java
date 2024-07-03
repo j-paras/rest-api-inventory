f7e58179b5ee60c8d3d2c9725ec5312f424770cf
Ashutosh Kushwaha <ashukushwaha201@gmail.com>
2024-07-03 23:18:08 +0530

    create setUp method for testing

--- src/test/java/com/bootcamp/inventory/InventoryApplicationTests.java
+++ src/test/java/com/bootcamp/inventory/InventoryApplicationTests.java
@@ -1,13 +1,35 @@
 package com.bootcamp.inventory;
 
+import com.bootcamp.inventory.entity.Inventory;
+import com.bootcamp.inventory.repository.InventoryRepository;
+import com.bootcamp.inventory.service.InventoryService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InventoryApplicationTests {
 
+	private InventoryRepository inventoryRepository;
+
+	private InventoryService inventoryService;
+
+	private Inventory existingInventory;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@BeforeEach
+	public void setup(){
+		existingInventory = new Inventory();
+		existingInventory.setId(1L);
+		existingInventory.setSku("SKU123");
+		existingInventory.setCreatedBy("user1");
+		existingInventory.setCost(50.0);
+		existingInventory.setSellingPrice(100.0);
+		existingInventory.setLocation("Warehouse A");
+		existingInventory.setStatus("MODIFIED");
+		existingInventory.setUpdatedAt(LocalDateTime.now());
+	}
 }
