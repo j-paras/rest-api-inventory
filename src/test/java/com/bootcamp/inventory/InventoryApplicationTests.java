1d1aa60045dbdf7293ef365ae73f1960c5267b7f
Ashutosh Kushwaha <ashukushwaha201@gmail.com>
2024-07-03 23:32:32 +0530

    create test for the modifiedStatus method

--- src/test/java/com/bootcamp/inventory/InventoryApplicationTests.java
+++ src/test/java/com/bootcamp/inventory/InventoryApplicationTests.java
@@ -5,23 +5,30 @@ import com.bootcamp.inventory.repository.InventoryRepository;
 import com.bootcamp.inventory.service.InventoryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class InventoryApplicationTests {
+import java.time.LocalDateTime;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class InventoryApplicationTests {
+
+	@Mock
 	private InventoryRepository inventoryRepository;
 
 	private InventoryService inventoryService;
 
 	private Inventory existingInventory;
 
-	@Test
-	void contextLoads() {
-	}
-
 	@BeforeEach
-	public void setup(){
+	public void setup() {
+		MockitoAnnotations.openMocks(this);
+
 		existingInventory = new Inventory();
 		existingInventory.setId(1L);
 		existingInventory.setSku("SKU123");
@@ -31,5 +38,28 @@ class InventoryApplicationTests {
 		existingInventory.setLocation("Warehouse A");
 		existingInventory.setStatus("MODIFIED");
 		existingInventory.setUpdatedAt(LocalDateTime.now());
+		when(inventoryRepository.findById(1L)).thenReturn(Optional.of(existingInventory));
+	}
+
+	@Test
+	public void testModifiedStatus_Sold() {
+		Inventory updateInventory = new Inventory();
+		updateInventory.setId(1L);
+		updateInventory.setStatus("SOLD");
+
+		inventoryService.modifiedStatus(updateInventory, existingInventory);
+
+		assertEquals("SOLD", existingInventory.getStatus());
+	}
+
+	@Test
+	public void testModifiedStatus_NotSold() {
+		Inventory updateInventory = new Inventory();
+		updateInventory.setId(1L);
+		updateInventory.setStatus("something_else");
+
+		inventoryService.modifiedStatus(updateInventory, existingInventory);
+
+		assertEquals("MODIFIED", existingInventory.getStatus());
 	}
 }
